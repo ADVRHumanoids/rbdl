fcc1be5d429dbcd175059856144d30d52b7ce863
tree-comexpress2 <alberobotics@iit.it>
2021-06-23 17:29:52 +0200

    test (example_urdfreader): test rbdl outputs

--- examples/urdfreader/example_urdfreader.cc
+++ examples/urdfreader/example_urdfreader.cc
@@ -23,20 +23,58 @@ int main (int argc, char* argv[]) {
 
 	Model* model = new Model();
 
-	if (!Addons::URDFReadFromFile ("./samplemodel.urdf", model, false)) {
-		std::cerr << "Error loading model ./samplemodel.urdf" << std::endl;
+    if (!Addons::URDFReadFromFile(argv[1], model, false)) {
+        std::cerr << "Error loading model " << argv[1] << std::endl;
 		abort();
 	}
 
-	VectorNd Q = VectorNd::Zero (model->dof_count);
-	VectorNd QDot = VectorNd::Zero (model->dof_count);
-	VectorNd Tau = VectorNd::Zero (model->dof_count);
-	VectorNd QDDot = VectorNd::Zero (model->dof_count);
+    std::cout << "initializing variables... " << std::endl;
+    double mass = 0;
+    Vector3d CoM = Vector3d::Zero();
+//    VectorNd Q = VectorNd::Zero (model->dof_count);
+    VectorNd Q = VectorNd::Ones (model->dof_count) * 1.57;
+    VectorNd QDot = VectorNd::Ones (model->dof_count) * 1.0;
+    VectorNd QDDot = VectorNd::Ones (model->dof_count) * 2.0;
+    VectorNd Tau = VectorNd::Zero (model->dof_count);
+    MatrixNd M;
+    Vector3d ref_point = Vector3d::Zero();
+    MatrixNd J;
+    std::cout << "Q: " << Q.transpose() << std::endl;
+    std::cout << "QDot: " << QDot.transpose() << std::endl;
+    std::cout << "QDDot: " << QDDot.transpose() << std::endl;
 
- 	ForwardDynamics (*model, Q, QDot, Tau, QDDot);
+    std::cout << "computing... " << std::endl;
+    // Fill robot mass
+    RigidBodyDynamics::Utils::CalcCenterOfMass(*model, Q, QDot, mass, CoM, nullptr, nullptr, false);
+    std::cout << "robot mass: " << mass << std::endl;
+    std::cout << "robot CoM: " << CoM.transpose() << std::endl;
 
-	std::cout << Q.transpose() << std::endl;
-	std::cout << QDDot.transpose() << std::endl;
+//    inverse dynamics
+    InverseDynamics (*model, Q, QDot, QDDot, Tau, nullptr);
+//    inertia matrix
+    M.setZero(model->dof_count, model->dof_count);
+    CompositeRigidBodyAlgorithm(*model, Q, M);
+//    jacobian
+    unsigned int id = model->GetBodyId("gripper_A");
+    J.setZero(6, model->dof_count);
+    CalcPointJacobian6D(*model, Q, id, ref_point, J);
+//forward kinematics
+    Matrix3d _tmp_matrix3d = RigidBodyDynamics::CalcBodyWorldOrientation(*model,
+                                                                Q,
+                                                                id);
+
+    Vector3d _tmp_vector3d = RigidBodyDynamics::CalcBodyToBaseCoordinates(*model,
+                                                                 Q,
+                                                                 id,
+                                                                 ref_point);
+
+    _tmp_matrix3d.transposeInPlace();
+
+    std::cout << "Tau (inv. dyn.): \n" << Tau.transpose() << std::endl;
+    std::cout << "Inertia matrix: \n" << M << std::endl;
+    std::cout << "Jacobian gripper_A: \n" << J << std::endl;
+    std::cout << "forward kinematics gripper_A: \n" << _tmp_vector3d.transpose() << std::endl;
+    std::cout << "forward kinematics orientation gripper_A: \n" << _tmp_matrix3d << std::endl;
 
 	delete model;
 
